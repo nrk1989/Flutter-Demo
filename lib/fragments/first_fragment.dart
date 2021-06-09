8c28cc9f6c35e9aa79d2a2982dfce92dc842844d
Rajkumar Nagarajan <rajkumar.nagarajan@imaginea.com>
2021-06-09 23:17:44 +0530

    # Initial commit

---
+++ lib/fragments/first_fragment.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FirstFragment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: new Text("First Fragment"),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:demo/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new HomePage(),
+    );
+  }
+}
